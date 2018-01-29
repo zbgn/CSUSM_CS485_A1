29642b73c21e8aaba9a0c2e1ac353d2e2f680084
Gysco <zackary.beaugelin@epitech.eu>
2018-01-29 13:08:36 -0800

    Update: GameScene1 Roll-A-Ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float mvHorizontal = Input.GetAxis ("Horizontal");
+		float mvVertical = Input.GetAxis ("Vertical");
+
+		Vector3 mv = new Vector3 (mvHorizontal, 0.0f, mvVertical);
+
+		rb.AddForce (mv * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		if (count <= 1) countText.text = "Coin: " + count.ToString ();
+		else countText.text = "Coins: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+	}
+}
