472b7ad6bf603cb62b2c644e105387e6c1142ecb
Gysco <zackary.beaugelin@epitech.eu>
2018-02-03 17:58:12 -0800

    Update: GameScene3 Menu

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
+	void Start ()
+	{
+		transform.Rotate(new Vector3(0, Random.Range(0, 180), 0));
+		transform.position = new Vector3(Random.Range(-9, 9), .5f, Random.Range(-9, 9));
+	}
 	void Update () 
 	{
 		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
